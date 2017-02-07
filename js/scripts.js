4ee018fb73cac06e5079e413016506300e148299
Oleg Torokhov <torokhov093@gmail.com>
2017-02-07 17:13:15 +0700

    add constructor 'View', method 'look'

--- js/scripts.js
+++ js/scripts.js
@@ -81,6 +81,20 @@ World.prototype.toString = function() {
   return output;
 }
 
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+
+
+View.prototype.look = function(dir) {
+  var target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target)) {
+    return charFromElement(this.world.grid.get(target));
+  } else {
+    return "#";
+  }
+}
 
 
 
