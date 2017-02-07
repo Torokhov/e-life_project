94c82422bacb3f83b202f53772fd2eb087a38517
Oleg Torokhov <torokhov093@gmail.com>
2017-02-07 15:42:05 +0700

    add obj 'directions'

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,17 @@ Grid.prototype.get = function(vector) {
 
 function Wall() {}
 
+var directions = {
+  "n": new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e": new Vector( 1, 0),
+  "se": new Vector( 1, 1),
+  "s": new Vector( 0, 1),
+  "sw": new Vector(-1, 1),
+  "w": new Vector(-1, 0),
+  "nw": new Vector(-1, -1)
+};
+
 function randomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
