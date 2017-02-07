37d33c1f483af1ecb8e3cedf305cd368ae6896ea
Oleg Torokhov <torokhov093@gmail.com>
2017-02-07 15:49:24 +0700

    add func 'elementFromChar'

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,16 @@ function BouncingCritter() {
   this.direction = randomElement(Object.keys(directions));
 }
 
-
+function elementFromChar(legend, ch) {
+  if (ch == " ") {
+    return null;
+  }
+  
+  var element = new legend[ch]();
+  element.originChar = ch;
+  
+  return element;
+}
 
 
 
