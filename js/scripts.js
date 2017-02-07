0aabac5dcc9794a34fe346fa9c0a1c3d63bace0f
Oleg Torokhov <torokhov093@gmail.com>
2017-02-07 13:10:47 +0700

    add methods 'set', 'get'

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ function Grid(width, height) {
   this.height = height;
   this.space = new Array(width * height);
 }
+
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+}
+
+Grid.prototype.get = function(vector) {
+  return this.space[vector.x + this.width * vector.y];
+}
